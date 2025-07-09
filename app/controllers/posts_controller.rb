245557df59d85512637c252219f29083621800b8
kei2kei <jianye.hongfu@gmail.com>
2025-07-09 20:27:17 +0900

    記事投稿機能ベース作成（デザインは未定）

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,45 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(created_at: :desc)
   end
+
+  def show
+    @post = find_post(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+    @post = find_post(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = 1
+    if @post.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def find_post(id)
+    Post.find(id)
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 end
