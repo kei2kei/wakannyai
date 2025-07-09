e2fdeb9345c7e83784ff7bd197149b94444595b2
kei2kei <jianye.hongfu@gmail.com>
2025-07-09 20:32:12 +0900

    Revert "記事投稿機能ベース作成（デザインは未定）"
    This reverts commit 245557df59d85512637c252219f29083621800b8.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :posts, only: %i[new create edit update show destroy]
+  resources :posts, only: %i[edit update show destroy]
   root "posts#index"
 end
