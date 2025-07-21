5f12aded87ad93da4336275dcd1716bbcb86e793
kei2kei <jianye.hongfu@gmail.com>
2025-07-21 11:18:08 +0900

    タグ名の検索機能実装

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,5 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+import TagSuggestController from "./tag_suggest_controller"
+application.register("tag_suggest", TagSuggestController)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   get 'user_sessions/create'
   get 'user_sessions/destroy'
   resources :posts, only: %i[new create edit update show destroy]
-  root "posts#index"
+  root 'posts#index'
+  get 'tags/search', to: 'tags#search'
   get 'login', to: 'user_sessions#new', as: :login
   post 'login', to: 'user_sessions#create'
   delete 'logout', to:'user_sessions#destroy', as: :logout
