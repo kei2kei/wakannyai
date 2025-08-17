590c642d907c38924a9447a5237f9e11d75d912d
kei2kei <jianye.hongfu@gmail.com>
2025-08-17 11:08:37 +0900

    Sorceryの削除とDeviseのインストールおよびUsersテーブルの競合カラム削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
-  authenticates_with_sorcery!
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:encrypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:encrypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:encrypted_password] }
 
   has_many :posts, dependent: :destroy
   has_many :git_hub_contributions, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'user_sessions/new'
   get 'user_sessions/create'
   get 'user_sessions/destroy'
