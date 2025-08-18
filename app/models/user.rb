79f77e328f452cf1e164df124ee2614c28ad24dc
kei2kei <jianye.hongfu@gmail.com>
2025-08-18 21:13:09 +0900

    モデル作成、ヘッダーリンクの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,20 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :rememberable, :omniauthable, omniauth_providers: %i[github]
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:encrypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:encrypted_password] }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:encrypted_password] }
 
   has_many :posts, dependent: :destroy
   has_many :git_hub_contributions, dependent: :destroy
+
+  private
+
+  def self.from_omniauth(auth)
+    find_or_create_by(provider: auth.provider, uid: auth.uid) do |user|
+      user.email = auth.info.email
+      user.name = auth.info.name
+    end
+  end
 end
