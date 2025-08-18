6362d4688ebc9868e9b3fb6e5ffe5295819b0531
kei2kei <jianye.hongfu@gmail.com>
2025-08-18 21:11:57 +0900

    provider,uidカラムの追加、不要なカラム削除

--- db/migrate/20250817014229_devise_create_users.rb
+++ db/migrate/20250817014229_devise_create_users.rb
@@ -5,11 +5,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
     change_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      # t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -33,8 +33,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       # t.datetime :locked_at
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :email,                unique: true
+    # add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
