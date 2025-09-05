920483299f3c059b63c272c647508feea9e0e6ed
kei2kei <jianye.hongfu@gmail.com>
2025-09-05 15:52:01 +0900

    ベストアンサー選出でポイント加算される際のレベルアップ実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -33,17 +33,28 @@ class CommentsController < ApplicationController
 
   def set_best_comment
     @comment = Comment.find(params[:id])
-    if @comment.post.best_comment.present? || current_user != @comment.post.user || @comment.post.user == @comment.user
+    comment_author = @comment.user
+    unless can_set_best_comment?
       redirect_to post_path(@comment.post), alert: '権限がありません。'
     end
 
     @comment.post.update(best_comment: @comment)
-    @comment.user.increment!(:points, 5)
+    comment_author.increment!(:points, 5)
+    comment_author.cat.update_level
     redirect_to post_path(@comment.post), notice: 'ベストアンサーを選びました。'
   end
 
   private
   def comment_params
     params.require(:comment).permit(:content, :parent_id).merge(post_id: params[:post_id])
+  end
+
+  def can_set_best_comment?(comment_author)
+    # 投稿の投稿者が選んでいるか
+    # まだベストアンサーが選ばれていないか
+    # 自分自身のコメントを選んでいないか
+    current_user == @comment.post.user &&
+      @comment.post.best_comment.blank? &&
+      current_user != comment_author
   end
 end
