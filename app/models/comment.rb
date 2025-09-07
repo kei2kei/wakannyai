9abfa71047ed95611c2d8504938bc94d6eb46829
kei2kei <jianye.hongfu@gmail.com>
2025-09-07 09:13:19 +0900

    コメントのモデルテスト

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   validates :content, presence: true, length: { maximum: 65_535 }
   belongs_to :post
   belongs_to :user
-  belongs_to :parent_comment, class_name: 'Comment', optional: true
+  belongs_to :parent_comment, class_name: 'Comment', foreign_key: :parent_id, optional: true
   has_many :replies, class_name: 'Comment', foreign_key: :parent_id, dependent: :destroy
   scope :best_answer, -> { find_by(is_best_answer: true) }
 end
