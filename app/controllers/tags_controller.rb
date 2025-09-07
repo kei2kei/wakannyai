b1082d1ea6878d4043a811090b2bcfd9b24acde9
kei2kei <jianye.hongfu@gmail.com>
2025-09-07 12:19:36 +0900

    タグ機能の改良

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,12 +1,17 @@
 class TagsController < ApplicationController
   def search
-    query = params[:query]
-    if query.present?
-      @tags = Tag.where("name Like ?", "#{query}%").limit(5)
-    else
-      @tags = Tag.none
-    end
-    render json: @tags.map { |tag| { id: tag.id, name: tag.name } }
+    q = (params[:q] || params[:query]).to_s.strip
+    return render json: [] if q.blank?
+
+    # ワイルドカードのエスケープ
+    pattern = ActiveRecord::Base.sanitize_sql_like(q.downcase) + "%"
+
+    tags = Tag.where("LOWER(name) LIKE ?", pattern)
+              .order(:name)
+              .limit(20)
+              .pluck(:id, :name)
+
+    render json: tags.map { |id, name| { value: name, id: id } }
   end
 
   private
