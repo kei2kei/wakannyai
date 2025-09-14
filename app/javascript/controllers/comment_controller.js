84e3c48a17f8f069d8141d8454295acf8f0ead3f
kei2kei <jianye.hongfu@gmail.com>
2025-09-15 01:23:48 +0900

    コメントフォームを付け加えるのではなく、返信を表示する設計に変更

--- app/javascript/controllers/comment_controller.js
+++ app/javascript/controllers/comment_controller.js
@@ -1,27 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["replyForm"]
-  connect(){
-    console.log("replyFormActivated")
-  }
-  async showReplyForm(event) {
-    const commentId = event.target.dataset.commentId;
-    const formContainer = this.element.querySelector(`#reply-form-${commentId}`);
-    if (formContainer) {
-      formContainer.remove();
-      return;
-    }
+  static targets = ["replyFormContainer"]
+  static values = {
+    unattachedBlobs: Array,
+    imagesAllowed: { type: Boolean, default: true }
+  };
 
-    try {
-      const response = await fetch(`/comments/new_reply?parent_id=${commentId}`);
-      if (!response.ok) {
-        throw new Error('Failed to fetch reply form.');
-      }
-      const html = await response.text();
-      this.element.insertAdjacentHTML('beforeend', html);
-    } catch (error) {
-      console.error("Error showing reply form:", error);
-    }
+  connect () {
+  }
+
+  toggleReply() {
+    this.replyFormContainerTarget.classList.toggle("hidden")
   }
 }
